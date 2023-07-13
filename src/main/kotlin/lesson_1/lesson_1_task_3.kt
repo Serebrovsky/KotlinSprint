e5934d06b712cf4e8ab16025c84be467d353b6a0
Serebrovsky Vladimir <nymphetamine2006@yandex.ru>
2023-07-14 00:11:57 +0300

    ks-1-3

---
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -0,0 +1,15 @@
+package lesson_1
+fun main() {
+    val year = 1961 //год полета Юрия Гагарина в космос
+    var hour = 9
+    var minute = 7
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour=10
+    minute=55
+
+    println("$hour:$minute")
+}
