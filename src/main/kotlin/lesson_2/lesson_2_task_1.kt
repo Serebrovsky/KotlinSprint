1b7969370e4c68a46560e1ba2d03cecb01648228
Serebrovsky Vladimir <nymphetamine2006@yandex.ru>
2023-07-16 17:49:31 +0300

    ks-2-1

---
+++ src/main/kotlin/lesson_2/lesson_2_task_1.kt
@@ -0,0 +1,13 @@
+package lesson_2
+
+fun main () {
+    val person1: Float = 3f
+    val person2: Float = 4f
+    val person3: Float = 3f
+    val person4: Float = 5f
+
+    //Вычисляем средний балл студентов:
+    val srBal: Float = (person1 + person2 + person3 + person4) / 4
+
+    println(srBal)
+}
