da08f07f55bc3a69ed275d4a35628e4d31971c38
Serebrovsky <138938322+Serebrovsky@users.noreply.github.com>
2023-07-17 10:41:46 +0300

    Delete lesson_1_task_1.kt

--- src/main/kotlin/lesson_1/lesson_1_task_1.kt
+++
@@ -1,9 +0,0 @@
-package lesson_1
-
-fun main(){
-
-    //Всего заказов:
-    var ourOrders:Int=75
-    //Благодарственное сообщение:
-    val helloMessage:String="Благодарим Вас за покупку. Надеемся, скоро увидимся снова!"
-}
