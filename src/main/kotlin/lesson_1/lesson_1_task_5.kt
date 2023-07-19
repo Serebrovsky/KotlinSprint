0311fb592d5536cb6fda003d0f115303810482e7
Serebrovsky Vladimir <nymphetamine2006@yandex.ru>
2023-07-19 15:11:16 +0300

    ks-1-5

--- src/main/kotlin/lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -3,16 +3,12 @@ package lesson_1
 fun main() {
 
     //Количество секунд, которые Гагарин провел в космосе:
-    val sumGag: Int = 32850//сек
-    val hour: Int = sumGag / 3600 //9часов
-    val hour1: Int = hour * 3600 //секунд в 9 часах
+    val totalSeconds: Int = 32850//сек
 
-    val ost = sumGag - hour1
+    val hour = totalSeconds / 3600 //целые часы
+    val min = (totalSeconds % 3600) / 60 //остаток в минутах
+    val sec = (totalSeconds % 3600) % 60 //остаток в секундах
 
-    val ost1 = ost/60 //7мин
-    val ost2 = ost1 * 60 //420сек
-    val ost3 = sumGag - hour1 - ost2 //30 сек
-    println("0$hour:0$ost1:$ost3")
-
-
+    val time = String.format("%02d:%02d:%02d", hour, min, sec)
+    println(time)
 }
