6f5298f033b799bb7c2bfd2b01663be856cbdc65
Serebrovsky Vladimir <nymphetamine2006@yandex.ru>
2023-07-25 17:07:49 +0300

    ks-4-2 correct

--- src/main/kotlin/lesson_4/lesson_4_task_2.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_2.kt
@@ -19,8 +19,10 @@ fun main() {
     val weightLoad2 = 50
     val sizeLoad2 = 100
 
-    println("Груз с весом $weightLoad1 и объемом $sizeLoad1 соответствует категории \"Average\": ${AVERAGE_WEIGHT_MIN <= weightLoad1 && weightLoad1 <= AVERAGE_WEIGHT_MAX && sizeLoad1 < SIZE}")
-    println("Груз с весом $weightLoad2 и объемом $sizeLoad2 соответствует категории \"Average\": ${AVERAGE_WEIGHT_MIN <= weightLoad2 && weightLoad2 <= AVERAGE_WEIGHT_MAX && sizeLoad2 < SIZE}")
+    println("Груз с весом $weightLoad1 и объемом $sizeLoad1 соответствует категории \"Average\": " +
+            "${AVERAGE_WEIGHT_MIN <= weightLoad1 && weightLoad1 <= AVERAGE_WEIGHT_MAX && sizeLoad1 < SIZE}")
+    println("Груз с весом $weightLoad2 и объемом $sizeLoad2 соответствует категории \"Average\": " +
+            "${AVERAGE_WEIGHT_MIN <= weightLoad2 && weightLoad2 <= AVERAGE_WEIGHT_MAX && sizeLoad2 < SIZE}")
 }
 
 const val AVERAGE_WEIGHT_MIN = 35
