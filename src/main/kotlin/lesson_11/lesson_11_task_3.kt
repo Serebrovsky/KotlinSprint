f906cdc888fd4f5d9692f10466fe3e5460f3a0e6
Serebrovsky Vladimir <nymphetamine2006@yandex.ru>
2023-09-18 16:12:01 +0300

    ks-11-3

--- src/main/kotlin/lesson_11/lesson_11_task_3.kt
+++ src/main/kotlin/lesson_11/lesson_11_task_3.kt
@@ -11,31 +11,77 @@ package lesson_11
 Рядом с аватаркой отображается текстовый бейдж с одним из возможных статусов говорящего: “разговаривает”,
 “микрофон выключен”, “пользователь заглушен”.
 После проектирования создай список объектов карточек с несколькими “комнатами” с произвольными данными.*/
-
-
 fun main(){
 
+    val room1 = Rooms(
+        roomId = 1,
+        cover = 344124,
+        name = "Волейбол",
+        idOfParticipants = listOf(1, 2, 3, 4, 5, 6, 7, 8),
+        imageOfParticipants = listOf(1231, 1231, 3442, 4573, 5644, 2345, 1244, 4544),
+        nicknameOfParticipants = listOf(
+            "vova749",
+            "vika1210",
+            "lola55",
+            "viktor89",
+            "egor",
+            "spike",
+            "misha",
+            "kirill"
+        ),
+        status = listOf("разговаривает", "микрофон выключен", "пользователь заглушен"),
+    )
+    val room2 = Rooms(
+        roomId = 2,
+        cover = 4534235,
+        name = "Литература",
+        idOfParticipants = listOf(2, 3, 6, 7, 8),
+        imageOfParticipants = listOf(4573, 5644, 2345, 1244, 4544),
+        nicknameOfParticipants = listOf(
+            "vova749",
+            "vika1210",
+            "lola55",
+            "viktor89",
+            "egor",
+        ),
+        status = listOf("разговаривает", "микрофон выключен", "пользователь заглушен"),
+    )
+    val room3 = Rooms(
+        roomId = 3,
+        cover = 4534243,
+        name = "Мотоциклы",
+        idOfParticipants = listOf(2, 3, 4),
+        imageOfParticipants = listOf(4573, 5644, 2345),
+        nicknameOfParticipants = listOf(
+            "vova749",
+            "vika1210",
+            "lola55",
+        ),
+        status = listOf("разговаривает", "микрофон выключен", "пользователь заглушен"),
+    )
+
+    println(room1.showNicknameAndStatus(5, 1))
+    println(room2.showNicknameAndStatus(0, 1))
+   println(room3.showNicknameAndStatus(1, 2))
+
+    val roomsList = listOf(room1, room2, room3) //не понял, что нужно сделать(
+
+
 }
-// Прошу прощения, но совершенно не чувствую до какой глубины мне нужно сделать это приложение.
-// Пока много что не чувствую при работе с классами. Подскажите в каком направлении идти, а то жесткий затуп
-class Room(
-    val nameMember: String,
-    val avatarMember: Int,
-    val userList: MutableList<User>, //хранятся объекты
-) {
-    fun showNicknameAndStatus(avatarOfUser: Int) {
-        userList.forEach{println(avatarOfUser(it))}
+
+class Rooms(
+    val roomId: Int, //ID комнаты
+    val cover: Int, //обложка
+    var name: String, //название комнаты
+    var idOfParticipants: List<Int>,//ID участников
+    var imageOfParticipants: List<Int>, //аватары участников
+    var nicknameOfParticipants: List<String>, //никнеймы участников
+
+    val status: List<String> = listOf("разговаривает",
+    "микрофон выключен", "пользователь заглушен"),
+        ){
+    fun showNicknameAndStatus (i: Int, s: Int){
+        println("Никнейм пользователя в комнате $name: ${nicknameOfParticipants[i]}, " +
+                "его статус: ${status[s]}")
     }
 }
-
-class User(
-    val avatarOfUser: Int,
-    val nickname: String,
-    val status: String,
-    )
-{fun status(){
-    println(status)
-}
-
-}
-
