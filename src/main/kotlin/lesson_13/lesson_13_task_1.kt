ea92ab552ce44d21438b53b5453af9eea0a58c53
Serebrovsky Vladimir <nymphetamine2006@yandex.ru>
2023-10-06 21:09:27 +0300

    ks-13-1

---
+++ src/main/kotlin/lesson_13/lesson_13_task_1.kt
@@ -0,0 +1,24 @@
+package lesson_13
+
+/*Задача 1 к Уроку 13
+Для программы телефонного справочника создай класс, который будет хранить имя, номер телефона и поле company.
+ Номер телефона – целочисленное значение. Нужно учесть ситуацию, что поле с компанией может оставаться
+ незаполненным. Поля не должны иметь никакой инициализации по умолчанию.*/
+
+fun main() {
+    val contact1 = Contact("Владимир", 89152095507)
+    println(contact1.name)
+    println(contact1.number)
+
+    val contact2 = Contact("Вика", 89154052714)
+    println(contact2.name)
+    println(contact2.number)
+
+    contact2.company = "ВНИИНМ"
+    println(contact2.company)
+}
+
+class Contact(var name: String, var number: Long) {
+    var company: String? = null
+
+}
