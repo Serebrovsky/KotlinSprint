b3dd9169e1422245acb54f82a0c533943a2a2edd
Serebrovsky Vladimir <nymphetamine2006@yandex.ru>
2023-11-26 14:45:06 +0300

    ks-13-5

--- src/main/kotlin/lesson_13/lesson_13_task_5.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_5.kt
@@ -1,4 +1,7 @@
 package lesson_13
+
+import java.lang.NumberFormatException
+
 /*Задача 5* к Уроку 13
 Скопируй класс из предыдущей задачи (без логики заполнения телефонной книги) и измени его название
 для избегания конфликтов.
@@ -7,13 +10,13 @@ package lesson_13
 Необходимо воспроизвести ошибку и вывести ее название в консоль.*/
 
 fun main() {
-
+    println("Введите имя:")
     val name = readln()
-    val number = readln().toLongOrNull()
-    if (number == null) {
-        println("Ошибка! Вы не ввели номер телефона!")
-    }
 
+    println("Введите номер телефона:")
+    val number = readln()
+
+    println("Введите название компании:")
     val company = readln()
 
     val contact1 = Contacttttt(name, number, company)
@@ -24,12 +27,23 @@ fun main() {
 
 class Contacttttt(
     var name: String,
-    var number: Long?,
+    numberString: String,
     var company: String? = null,
 ) {
+    var numberLong: Long? = null
+
+    init {
+        try {
+            numberLong = numberString.toLong()
+
+        } catch (e: NumberFormatException) {
+            println("Ошибка! Вы не ввели номер телефона!")
+
+        }
+    }
 
     fun printContact() {
-        println("Имя: $name, номер телефона: $number, компания: ${company ?: "<не указано>"}")
+        println("Имя: $name, номер телефона: ${numberLong?:"<не указано>"}, компания: ${company ?: "<не указано>"}")
     }
 
 
