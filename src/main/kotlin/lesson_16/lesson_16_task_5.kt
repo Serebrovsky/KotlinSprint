1ff1b73e06cf2ba431885130e6b257f950090540
Serebrovsky Vladimir <nymphetamine2006@yandex.ru>
2024-02-08 16:43:37 +0300

    ks-16-5

---
+++ src/main/kotlin/lesson_16/lesson_16_task_5.kt
@@ -0,0 +1,69 @@
+package lesson_16
+
+/*Задача 5* к Уроку 16
+Для игры требуется создать класс игрока. С полями: имя, здоровье и сила удара.
+Здоровье должно изменяться только через методы получения урона и лечения.
+
+Добавить приватный метод, который вызывается, если при получении урона заканчивается здоровье.
+Смерть обнуляет силу удара, здоровье и не позволяет лечиться. Сымитируй бой,
+вызвав несколько раз методы урона и лечения. В конце игрок должен умереть.*/
+fun main() {
+    val player1 = Player("Владимир")
+    val player2 = Player("Света")
+
+    //получили урон:
+    player1.takingDamage()
+    player1.takingDamage()
+    player1.takingDamage()
+    player1.takingDamage()
+
+
+
+    player1.therapy()
+
+    //статистика:
+    player1.status()
+
+}
+
+
+class Player(val name: String) {
+    private var health = 100
+    var forceOfHit = 100
+    private var isDeath = false
+
+    fun status() {
+        println("СТАТИСТИКА:\nИмя: $name, здоровье: $health, сила удара: $forceOfHit")
+    }
+
+    fun takingDamage(): Int { //получение урона
+        health -= 25
+        println("$name, -25 здоровья: $health")
+        if (health <= 0) {
+            death()
+        }
+        return health
+    }
+
+    fun therapy(): Int { //лечение
+        if (!isDeath) {
+            health += 25
+            println("$name, +25 лечения: $health")
+
+        } else {
+            println("Невозможно лечение! Вы мертвы!")
+        }
+        return health
+    }
+
+
+    private fun death() {
+        forceOfHit = 0
+        health = 0
+        isDeath = true
+
+        println("$name, вы умерли! Ваше здоровье: $health, сила удара: $forceOfHit")
+
+    }
+
+}
